11eecf7bbb2bc05a1f0d68c68677074235f0ccff
uhalexz_ <152796233+uhAlexz@users.noreply.github.com>
2024-02-24 10:43:45 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.environ['TOKEN']
+
+
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot is loaded and running.")
+
+
+bot.run()
